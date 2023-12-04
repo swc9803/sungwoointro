e1af4cc4c4e234d72cf97e5a68ce3823f477ea1f
swc9803 <swc9803@gmail.com>
2023-12-04 18:08:28 +0900

    reset project

--- app.vue
+++ app.vue
@@ -1,16 +1,8 @@
 <template>
-  <div class="aoo">
-    <!-- <Morphing />
-    <Open /> -->
-    <TunnelScroll />
-  </div>
+  <div class="app"></div>
 </template>
 
 <script setup>
-// import Morphing from "@/components/Morphing.vue";
-// import Open from "@/components/Open.vue";
-import TunnelScroll from "@/components/TunnelScroll.vue";
-
 const onResize = () => {
   const vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
@@ -23,11 +15,6 @@ onMounted(() => {
 </script>
 
 <style lang="scss">
-.app {
-  position: fixed;
-  width: 100%;
-  height: calc(var(--vh) * 100);
-  background: #151515;
-  z-index: 1;
-}
+// .app {
+// }
 </style>
