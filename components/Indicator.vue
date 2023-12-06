0ff4f18ca4c780cfac3431ef9429c17b6a3db0a5
swc9803 <swc9803@gmail.com>
2023-12-06 18:34:35 +0900

    indicator mousemove 제거 및 우측 화단에 고정

--- components/Indicator.vue
+++ components/Indicator.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="indicator">
     <svg
       ref="progressIndicatorRef"
       width="100"
@@ -34,79 +34,84 @@ const progressIndicatorRef = ref();
 const circleLength = ref(2 * Math.PI * 49);
 const dashOffset = ref(circleLength.value);
 
-const onMouseMove = (e) => {
-  circleRef.value.style.top = `${e.clientY}px`;
-  circleRef.value.style.left = `${e.clientX}px`;
-  progressIndicatorRef.value.style.top = `${e.clientY}px`;
-  progressIndicatorRef.value.style.left = `${e.clientX}px`;
-};
-
 const onScroll = () => {
-  const height =
-    document.documentElement.scrollHeight -
-    document.documentElement.clientHeight;
-  const scrollTop = window.scrollY || document.documentElement.scrollTop;
+  if (matchMedia("screen and (min-width: 480px)").matches) {
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
 
-  waveRef.value.style.top = `${100 - (scrollTop / height) * 100}%`;
-  gsap.to(dashOffset, {
-    value: circleLength.value * (1 - scrollTop / height),
-    duration: 0.5,
-    ease: "power1.inout",
-  });
+    waveRef.value.style.top = `${100 - (scrollTop / height) * 100}%`;
+    gsap.to(dashOffset, {
+      value: circleLength.value * (1 - scrollTop / height),
+      duration: 0.5,
+      ease: "power1.inout",
+    });
+  }
 };
 
 onMounted(() => {
-  window.addEventListener("mousemove", onMouseMove);
   window.addEventListener("scroll", onScroll);
 });
 </script>
 
 <style lang="scss" scoped>
-svg {
+.indicator {
   position: fixed;
-  top: -80px;
-  left: -80px;
-  transform: translate(-50%, -50%) rotate(-90deg);
-  pointer-events: none;
-  // 크기 더 작게
-  // 모바일 none
-}
-.cursor {
-  position: fixed;
-  width: 60px;
-  height: 60px;
-  transform: translate(-50%, -50%);
-  border: 2px solid #ffffff;
-  border-radius: 50%;
-  overflow: hidden;
-  pointer-events: none;
-  // 모바일 none
-  .wave {
-    position: absolute;
-    top: 100%;
-    left: -50%;
-    width: 200%;
-    height: 200%;
-    border-radius: 38%;
-    background: rgb(187, 187, 187);
-    transition: all 0.3s ease;
-    animation: wave 4s linear infinite;
-    &::after {
-      content: "";
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      background: white;
-      border-radius: 38%;
-      animation: wave 2s linear infinite reverse;
+  bottom: 2vw;
+  right: 2vw;
+  svg {
+    transform: rotate(-90deg);
+    pointer-events: none;
+    @media (max-width: 480px) {
+      display: none;
     }
   }
-  @keyframes wave {
-    0% {
-      transform: rotate(0deg);
+  .cursor {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate3d(-50%, -50%, 0);
+    // bottom: 2vw;
+    // right: 2vw;
+    width: 60px;
+    height: 60px;
+    // transform: translate3d(-50%, -50%, 0);
+    border: 2px solid #ffffff;
+    border-radius: 50%;
+    overflow: hidden;
+    pointer-events: none;
+    z-index: 1;
+    @media (max-width: 480px) {
+      display: none;
     }
-    100% {
-      transform: rotate(360deg);
+    .wave {
+      position: absolute;
+      top: 100%;
+      left: -50%;
+      width: 200%;
+      height: 200%;
+      border-radius: 38%;
+      background: rgb(187, 187, 187);
+      transition: all 0.3s ease;
+      animation: wave 4s linear infinite;
+      &::after {
+        content: "";
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background: white;
+        border-radius: 38%;
+        animation: wave 2s linear infinite reverse;
+      }
+    }
+    @keyframes wave {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
     }
   }
 }
