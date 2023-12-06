9eee027f2adeee4064e4a0b15fd5568a670bbb7b
swc9803 <swc9803@gmail.com>
2023-12-07 01:38:11 +0900

    hover 이벤트 추가, 스크롤 트리거 추가

--- components/section/Skills.vue
+++ components/section/Skills.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="container">
     <h1>Skills</h1>
-    <div class="logo-container">
+    <div ref="containerRef" class="logo-container">
       <div
         v-for="item in skills"
         :ref="skillRef"
         :key="item.id"
         class="logo-wrapper"
       >
-        <img :src="item.src" :alt="item.src" />
+        <img :src="item.src" :alt="item.name" @load="onLoad" />
+        <p>{{ item.name }}</p>
       </div>
     </div>
   </div>
@@ -16,6 +17,8 @@
 
 <script setup>
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import VueLogo from "@/assets/img/skills/vue-logo.webp";
 import TypescriptLogo from "@/assets/img/skills/ts-logo.webp";
 import ReactLogo from "@/assets/img/skills/react-logo.webp";
@@ -28,6 +31,8 @@ import GsapLogo from "@/assets/img/skills/gsap-logo.webp";
 import PostmanLogo from "@/assets/img/skills/postman-logo.webp";
 import FigmaLogo from "@/assets/img/skills/figma-logo.webp";
 import BlenderLogo from "@/assets/img/skills/blender-logo.webp";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const skills = [
   { name: "Vue/Nuxt", src: VueLogo },
@@ -44,18 +49,28 @@ const skills = [
   { name: "Blender", src: BlenderLogo },
 ];
 
+const containerRef = ref();
+
 const skillArray = ref([]);
 const skillRef = (el) => skillArray.value.push(el);
 
-onMounted(() => {
-  gsap.from(skillArray.value, {
-    opacity: 0,
-    scale: 0,
-    ease: "back",
-    stagger: 0.1,
-    duration: 1.2,
-  });
-});
+const loadedImgCount = ref(0);
+const onLoad = () => {
+  loadedImgCount.value++;
+  if (loadedImgCount.value > 11) {
+    gsap.from(skillArray.value, {
+      scrollTrigger: {
+        trigger: containerRef.value,
+        start: "start 60%",
+      },
+      opacity: 0,
+      scale: 0,
+      ease: "back",
+      stagger: 0.1,
+      duration: 1.2,
+    });
+  }
+};
 </script>
 
 <style lang="scss" scoped>
@@ -82,17 +97,36 @@ $colors: #c8ffcd, #c2e6ff, #bffff4, #ffd9d2, #ffe3fc, #feffd0, #f7ffc7, #ffffff,
     width: 100%;
     margin: 0 auto;
     .logo-wrapper {
+      position: relative;
       border-radius: 50%;
       overflow: hidden;
-      cursor: pointer;
       transition: filter 0.5s;
       @for $i from 1 through length($colors) {
         &:nth-child(#{$i}):hover {
           filter: drop-shadow(0 0 1em nth($colors, $i));
+          img {
+            filter: brightness(30%);
+          }
+          p {
+            opacity: 1;
+          }
         }
       }
       img {
         width: 100%;
+        transition: filter 0.5s;
+      }
+      p {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate3d(-50%, -50%, 0);
+        color: white;
+        font-size: 1.5em;
+        font-weight: 700;
+        letter-spacing: 0.2em;
+        opacity: 0;
+        transition: opacity 0.5s;
       }
     }
   }
