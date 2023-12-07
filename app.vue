39f75dd1421fae807e445fdcf48e32430052f032
swc9803 <swc9803@gmail.com>
2023-12-07 17:09:38 +0900

    헤더 제거

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="app">
-    <Header />
     <Intro />
     <Skills />
     <Projects />
@@ -10,20 +9,18 @@
 </template>
 
 <script setup>
-import Header from "./components/Header.vue";
 import Intro from "./components/section/Intro.vue";
 import Skills from "./components/section/Skills.vue";
 import Projects from "./components/section/Projects.vue";
 import Information from "./components/section/Information.vue";
 import Indicator from "./components/Indicator.vue";
+
 const onResize = () => {
   const vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 };
 
 onMounted(() => {
-  // console.clear();
-
   onResize();
   window.addEventListener("resize", onResize);
 });
