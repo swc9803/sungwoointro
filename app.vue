83c2204895b5ce1eb1e0f4420c71e78589d70b41
swc9803 <swc9803@gmail.com>
2023-12-08 16:15:04 +0900

    불필요한 코드 제거

--- app.vue
+++ app.vue
@@ -14,19 +14,4 @@ import Skills from "./components/section/Skills.vue";
 import Projects from "./components/section/Projects.vue";
 import Information from "./components/section/Information.vue";
 import Indicator from "./components/Indicator.vue";
-
-const onResize = () => {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-};
-
-onMounted(() => {
-  onResize();
-  window.addEventListener("resize", onResize);
-});
 </script>
-
-<style lang="scss">
-// .app {
-// }
-</style>
